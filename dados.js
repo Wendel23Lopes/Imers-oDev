77fac6c715587a7c5572946e02bbc4ff73db122b
Wendel23Lopes <wendeloliveiralopes@gmail.com>
2024-09-07 18:00:02 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,29 @@
+let dados = [
+   {    titulo : "Honda Cg 160" , 
+        descricao : "Linha mais vendida do país, as motos CG contam com um motor de 160cm³. Integrando esse time, a CG 160 Titan alia alto rendimento com baixo consumo de combustível, surpreendendo com um excelente custo-benefício." , 
+        link : "https://www.honda.com.br/motos/street/city/cg-160-fan" ,
+        tags : "cg 160 fan titan start"
+    } , 
+{   titulo : "Honda Cg 150" ,
+    descricao : "Primeira motocicleta bicombustível a chegar no mercado brasileiro, é voltada para uso urbano, locomoção no dia a dia e lazer nos finais de semana. Para quem valoriza tecnologia, estilo e, sobretudo, diferenciação." ,
+    link : "https://www.webmotors.com.br/motos/estoque/honda/cg-150-titan-ks" ,
+    tags : "cg 150 "
+} , 
+{   titulo : "Yamaha Fazer 250" ,
+    descricao : "A Yamaha lançou a Fazer 250 ABS no Salão Duas Rodas de 2017, que aconteceu em São Paulo. A motocicleta chegou já no modelo 2018 e veio totalmente renovada em comparação a antiga geração Fazer, mantendo apenas o motor." ,
+    link : "https://www.yamaha-motor.com.br/fazer-fz25-abs/product/30095?srsltid=AfmBOopsE-D1LIi3-P325Z9jbVcq1yJuXsjOLCKidrqPQ4__o11vOis_" ,
+    tags : "fazer 250 "
+} ,
+{   titulo : "Honda Cg 125" ,
+    descricao : "Honda CG é um best-seller global em toda a história da indústria (de carros e de motos). Só no Brasil, desde 1976, são quase 7 milhões de CG 125, sem contar a CG 150 (agora 160) que começou a ser fabricada em 2004 e já soma mais de 4,8 milhões de motos. Perceba que estamos falando apenas do Brasil, mas a linha CG 125 também é vendida noutros países da Ásia" ,
+    link : "https://www.motonline.com.br/guia-de-motos/honda/cg-125" ,
+    tags : "cg 125 pedal"
+
+},
+{   titulo : "Honda PCX 150" ,
+    descricao : "Honda PCX é racionalidade. Essa é a questão principal quando se aborda o tema do transporte individual nas grandes cidades. Está cada vez mais difícil para o habitante desses centros se cumprir as distâncias em tempo hábil quando é necessário deslocar-se ao trabalho ou para qualquer outra atividade corriqueira diária que requer deslocamento no espaço urbano" ,
+    link : "https://www.motonline.com.br/guia-de-motos/honda/pcx" ,
+    tags : "pcx 150 honda"
+
+}
+]
